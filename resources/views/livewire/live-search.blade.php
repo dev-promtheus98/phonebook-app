c11dcd3a37375679728f4f97c34324d48f6f1d63
dev-promtheus98 <ozh98@protonmail.com>
2020-06-17 00:33:36 +0000

    Initial project

---
+++ resources/views/livewire/live-search.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>Find a Contact</h1>
+
+    <div class="input-group mb-3">
+        <input type="text" wire:model="name" class="form-control" placeholder="Friend's name" aria-label="Recipient's username" aria-describedby="basic-addon2" autocomplete="off">
+        <div class="input-group-append">
+            <span class="input-group-text" id="basic-addon2">📘</span>
+        </div>
+    </div>
+</div>
